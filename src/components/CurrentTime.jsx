36a0890df1c641fc15da2ae529d5eed1c234aaf6
Muhammad Awais <kingyt6624@gmail.com>
2025-06-01 16:34:56 +0500

    updated

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -1,6 +1,15 @@
-const CurrentTime = () => {
-  let time = new Date();
+import { useEffect, useState } from "react";
 
+const CurrentTime = () => {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Force a re-render by updating the state
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className="current-time text-success pt-3 pb-3 fs-3 d-flex justify-content-center">
       <p>
